ed054dad6952b3ef044dd3fa77f0778baccea6f4
Mbassi Japhet <mbassijaphet@gmail.com>
2021-08-24 16:23:55 +0100

    Added list/array of event objects to display using events-list component

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -3,22 +3,27 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'event-thumbnail',
   template: `
-<div>
-    <h1>Upcomming Angular Events</h1>
-    <hr>
-    <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+    <div>
+        <div class="well hoverwell thumbnail">
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div>Time: {{event?.time}}</div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pas-left">{{event?.location?.city}}, {{event.location?.country}}</span>
+            </div>
+            <div *ngIf="event.onlineUrl">
+              <span>Online URL: {{event?.onlineUrl}}</span>
+            </div>
         </div>
     </div>
-</div>
-`,
+  `,
+  styles: [`
+    .thumbnail { min-height: 210px; }
+    .pas-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 export class EventThumbnailComponent implements OnInit {
 
