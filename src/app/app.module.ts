6c5063727409a5639dd539dd150808e8ad8e752b
Mbassi Japhet <mbassijaphet@gmail.com>
2021-09-23 18:24:38 +0100

    Created user profile page
    Not displaying for some reason

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { appRoutes } from './routes';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
-import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
@@ -13,11 +13,12 @@ import { Error404Component } from './error-page/error-page.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { CreateEventComponent, checkDirtyState } from './events/create-event.component';
 import { EventListResolver } from './events/events-list-resolver.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
