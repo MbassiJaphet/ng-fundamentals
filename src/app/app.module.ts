834535edd77361e8ee8727c1364b1b7b3925b33e
Mbassi Japhet <mbassijaphet@gmail.com>
2021-09-23 18:57:12 +0100

    Added import barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { appRoutes } from './routes';
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
-import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
-import { EventService } from './events/shared/event.service';
+import { NavBarComponent } from './shared/index';
+import { EventService } from './events/shared/index';
+import { Error404Component } from './errors/index';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  checkDirtyState,
+  EventListResolver,
+  EventRouteActivator,
+} from './events/index';
+
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { Error404Component } from './errors/error-404/error-404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { CreateEventComponent, checkDirtyState } from './events/create-event.component';
-import { EventListResolver } from './events/events-list-resolver.service';
-import { RouterModule } from '@angular/router';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   imports: [
