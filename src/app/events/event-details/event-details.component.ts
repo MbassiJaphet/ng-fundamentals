1f6e8b60165616c20dac922df4bc4e32fbc9d090
Mbassi Japhet <mbassijaphet@gmail.com>
2021-09-28 18:56:18 +0100

    Can now sort sessions (by name, votes) and filter sessions (by level)

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -13,6 +13,8 @@ export class EventDetailsComponent implements OnInit {
   public event!: IEvent;
   public addMode: boolean = false;
   public filterBy: String = 'all';
+  public sortBy: String = 'votes';
+
   constructor(private eventService: EventService,
               private route: ActivatedRoute) { }
 
--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -1,10 +1,41 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
 import { ISession } from '../../shared/index'
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges{
   @Input() sessions!:ISession[];
+  @Input() filterBy!:String;
+  @Input() sortBy!: String;
+  public visibleSessions: ISession[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByVotesAsc) : this.visibleSessions.sort(sortByVotesDesc);
+    };
+  }
+
+  filterSessions(filter: String) {
+    if(filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    }   else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
+  }
+}
+
+
+function sortByVotesAsc(s1: ISession, s2: ISession) {
+  if(s1.name > s2.name) { return 1; }
+  else if(s1.name === s2.name) { return 0; }
+  else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
