1310a039d7a378ba18d641288217ffada9d033ef
Mbassi Japhet <mbassijaphet@gmail.com>
2021-10-03 23:01:26 +0100

    Configured local server to handle http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { appRoutes } from './routes';
 import { NavBarComponent } from './shared';
@@ -35,7 +36,8 @@ declare let toastr: Toastr;
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations: [
     EventsAppComponent,
