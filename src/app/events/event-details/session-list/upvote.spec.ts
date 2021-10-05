4c1f8b5ca723a0bd59e10adc4557648306ca3ab1
Mbassi Japhet <mbassijaphet@gmail.com>
2021-10-05 16:20:50 +0100

    Test mock API calls fro mVoterService

--- src/app/events/event-details/session-list/upvote.spec.ts
+++ src/app/events/event-details/session-list/upvote.spec.ts
@@ -15,10 +15,28 @@ describe('VoterService Test', () => {
             var session = <ISession> { id: 6,  voters: ['joe', 'john']}
             mockHttp.delete.and.returnValue(of(false));
 
-            voterService.deleteVoter(session.id, session, 'joe');
+            voterService.deleteVoter(3, session, 'joe');
 
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe('john')
+        })
+
+        it('should call http.delete with the right URL', () => {
+            var session = <ISession> { id: 6,  voters: ['joe', 'john']}
+            mockHttp.delete.and.returnValue(of(false));
+
+            voterService.deleteVoter(3, session, 'joe');
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+        })
+
+        it('should call http.post with the right URL', () => {
+            var session = <ISession> { id: 6,  voters: ['john']}
+            mockHttp.post.and.returnValue(of(false));
+
+            voterService.deleteVoter(3, session, 'joe');
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
         })
     })
 })
